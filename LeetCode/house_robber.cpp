3bcd55ef63d26f3c19545bcdcde6f9bb2ff04599
brainuser5705 <codeuser5705@outlook.com>
2024-01-11 10:44:18 -0500

    Update house_robber.cpp

--- LeetCode/house_robber.cpp
+++ LeetCode/house_robber.cpp
@@ -25,3 +25,37 @@ public:
 
     }
 };
+
+// Don't need maxAmount or to copy nums vector
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+
+        if (nums.size() == 1){
+            return nums[0];
+        }
+
+        int nonAdjMax = nums[0];
+        int adjMax = nums[1];
+
+        int amt;
+        for (int i = 2; i < nums.size(); i++){
+
+            // get the max amount at current position
+            // note that nonAdjMax has not been updated yet
+            amt = nums[i] + nonAdjMax;
+            
+            // update nonAdjMax with old adjMax first
+            nonAdjMax = max(nonAdjMax, adjMax);
+
+            // finally update the adjMax
+            adjMax = amt;
+
+        }
+
+
+        return max(nonAdjMax, adjMax);
+        
+    }
+};
